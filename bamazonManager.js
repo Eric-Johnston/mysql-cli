6bf76da0a89818adff596e08d5305210b01bfec1
Eric J <ejohnston092@gmail.com>
2018-07-19 18:24:15 -0400

    manager module in progress

--- bamazonManager.js
+++ bamazonManager.js
@@ -15,10 +15,10 @@ connection.connect(function(err){
     console.log("\n----------------------");
     console.log("Connection successful!");
     console.log("----------------------\n");
-    managerCli();
+    managerInterface();
 });
 
-function managerCli(){
+function managerInterface(){
     inquirer.prompt([{
             name: "managerTasks",
             type: "list",
@@ -26,6 +26,63 @@ function managerCli(){
             choices: 
             ["View Products", "View Low Inventory", "Add to Inventory", "Add Product"]
         }]).then(function(answer){
-            switch (answer)
-        })
+
+            switch (answer.managerTasks){
+
+                case "View Products":
+                    viewProducts();
+                    break;
+
+                case "View Low Inventory":
+                    lowInventory();
+                    break;
+
+                case "Add to Inventory":
+                    addInventory();
+                    break;
+
+                case "Add Product":
+                    addProduct();
+                    break;
+
+                default:
+                    viewProducts();
+            };
+        });
+};
+
+function viewProducts(){
+    var query = "SELECT * FROM products;"
+    connection.query(query, function(err, res){
+        if(err) throw err;
+        for(i = 0; i < res.length; i++){
+            
+            var productName = res[i].product_name;
+            var productId = res[i].item_id;
+            var productPrice = res[i].price;
+            var productStock = res[i].stock_quantity;
+            console.log("---------------------")
+            console.log("PRODUCT NAME: " + productName + " | PRODUCT ID: " + productId + " | PRICE: " + productPrice + " | STOCK: " + productStock);
+            console.log("---------------------");
+        };
+        managerInterface();
+    });
+};
+
+function lowInventory(){
+    var query = "SELECT * FROM products WHERE stock_quantity < 50;";
+    connection.query(query, function(err, res){
+        if(err) throw err;
+        for(i = 0; i < res.length; i++){
+
+            var productName = res[i].product_name;
+            var productId = res[i].item_id;
+            var productPrice = res[i].price;
+            var productStock = res[i].stock_quantity;
+            console.log("---------------------")
+            console.log("PRODUCT NAME: " + productName + " | PRODUCT ID: " + productId + " | PRICE: " + productPrice + " | STOCK: " + productStock);
+            console.log("---------------------");
+            managerInterface();
+        }
+    })
 }
